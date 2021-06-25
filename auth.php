90c5b1af34faaaff131766a55ee8b4b9c32d7a87
EmberNya <54239712+l1fsec@users.noreply.github.com>
2021-06-25 18:08:02 +0200

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,22 +1,13 @@
 <?php
 session_start();
 
-//Připojení databáze
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'bot';
-$DATABASE_PASS = '30032';
-$DATABASE_NAME = 'phplogin';
-// Vyzkoušej připojení
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if ( mysqli_connect_errno() ) {
-	exit('Připojení se nezdařilo na: ' . mysqli_connect_error());
-}
-//Kontrola dat
+//Kontrola dat, cerpa se to z inputu z atirbutu name="name"
 if ( !isset($_POST['name'], $_POST['psw']) ) {
 
-	// Data nepřístupná
+	// Data nepřístupná, nic jsme nezadali a proto to neodesle
 	exit('Prosím vyplňte všechna pole!');
 }
+//.....................Tohle resi kontrolu, selectujeme data................................
 if ($stmt = $con->prepare('SELECT id, psw FROM accounts WHERE name = ?')) {
 	$stmt->bind_param('s', $_POST['name']);
 	$stmt->execute();
@@ -24,17 +15,22 @@ if ($stmt = $con->prepare('SELECT id, psw FROM accounts WHERE name = ?')) {
 	if ($stmt->num_rows > 0) {
 	$stmt->bind_result($id, $psw);
 	$stmt->fetch();
+//--------------------------------------------------------------------------
+
 
 	if ($_POST['psw'] === $psw) { //Uspěšně připojeno
+		//regenuruji session
 		session_regenerate_id();
 		$_SESSION['loggedin'] = TRUE;
 		$_SESSION['name'] = $_POST['name'];
+		//tady budeme potrebovat neco udelat, ckj.
 		$_SESSION['id'] = $id;
-		header('Location: logged/home.php');
+		header('Location: prihlaseno.php');
 	}
 	}
 } else {
-	header('Location: menu/login.html');
+	//tohle te odesle zpatky na index, pokud jsou wrong credentials
+	header('Location: index.html');
 }
 
 
